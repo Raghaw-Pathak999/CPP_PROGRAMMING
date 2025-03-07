8a2fbe59964a7e3953dcfb069d1f940256ed6248
RAGHAW PATHAK <raghawpathak001@gmail.com>
2025-03-07 17:01:23 +0530

    Updated code

---
+++ 03_Operators/06_Increment_Decrement/03_Pre_Decrement.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5;   // a = 5
+    int b = --a; // b 1 -5 = 4 // a =4
+    cout << "a: " << a << endl;
+    cout << "b: " << b << endl;
+
+    return 0;
+}

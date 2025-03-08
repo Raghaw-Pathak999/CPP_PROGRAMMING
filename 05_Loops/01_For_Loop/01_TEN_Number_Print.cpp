90061b1d75590837e595cb1cab6dc560c1427f04
RAGHAW PATHAK <raghawpathak001@gmail.com>
2025-03-08 15:45:43 +0530

    Updated code

---
+++ 05_Loops/01_For_Loop/01_TEN_Number_Print.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+
+    for (i = 1; i <= 5; i++)
+    {
+        cout << i;
+    }
+
+    // 1
+    //
+    //
+    //
+    //
+    //
+
+    return 0;
+}
